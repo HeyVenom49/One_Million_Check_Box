f44d80930692e00daff27492f3270315e0919ebc
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-06-24 01:33:02 +0530

    Implement checkbox state management with Socket.IO and update HTML to fetch initial state

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import path from "node:path";
 import express from "express";
 import { Server } from "socket.io";
 
+const CHECKBOX_SIZE = 100;
+const state = {
+  checkboxes: new Array(CHECKBOX_SIZE).fill(false),
+};
+
 async function main() {
   const PORT = 8010;
 
@@ -18,13 +23,18 @@ async function main() {
     console.log(`Socket connected`, { id: socket.id });
     socket.on("client:checkbox:change", (data) => {
       console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
-      io.emit("server:checkbox:change", data);
+      const { index, checked } = data;
+      state.checkboxes[index] = checked;
+      io.emit("server:checkbox:change", { index, checked });
     });
   });
 
   // Express Handler
   app.use(express.static(path.resolve("./public")));
   app.get("/health", (req, res) => res.json({ healthy: true }));
+  app.get("/checkboxes", (req, res) =>
+    res.json({ checkboxes: state.checkboxes }),
+  );
 
   server.listen(PORT, () => {
     console.log(`Server is running on the http://localhost:${PORT}`);
